90a50119cfc2714331e06e6089071f668b356872
Luka <lzunic99@gmail.com>
2020-12-10 23:14:59 +0100

    Binary Search Tree started

--- Binary-Trees/BST.h
+++ Binary-Trees/BST.h
@@ -6,7 +6,6 @@ class Node{
     public:
         T _value;
         Node* _parent = nullptr, *_left_child = nullptr, *_right_child = nullptr;
-    
         Node(T value):_value{value}{}
 };
 
@@ -14,7 +13,6 @@ template<typename T>
 class BST{
     public:
         Node<T>* _root;
-
         BST(T root_value):_root{new Node<T>(root_value)}{}
 
 };
